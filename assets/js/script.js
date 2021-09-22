d7076016987870b2cbd5028c093c5b7dec87c36d
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-09-22 11:52:22 -0400

    resolved form issues. created ingredient button creation and deletion.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,29 @@ let veganCheckEl = document.getElementById('vegan');
 let kosherCheckEl = document.getElementById('kosher');
 let searchButtonEl = document.getElementById('search-button');
 let ingredientBtnsEl = document.getElementById('ingredient-btns');
+let recipeAreaEl = document.getElementById('recipe-cards');
+let searchAreaEl = document.getElementById('search-area');
+let ingredbtnArr = [];
 
-// create a function for submit for the input field that will create deletable ingredient buttons
 
+// create a function for submit for the input field that will create deletable ingredient buttons giv buttons class of "ingredBtns"
+searchAreaEl.addEventListener('submit', function(e){
+    e.preventDefault();
+    let newBtn = document.createElement("button")
+    newBtn.innerHTML = searchInputEl.value;
+    newBtn.setAttribute('class', 'ingredBtns')
+    ingredientBtnsEl.appendChild(newBtn);
+    searchInputEl.value = "";
+})
 
+// add event listener for ingredient buttons - if class === "ingredBtns" delete on press.
+
+ingredientBtnsEl.addEventListener('click', function(e){
+    e.stopPropagation();
+    if (e.target.className === "ingredBtns") {
+        e.target.remove();
+    }
+})
 
 // create event listener for search button
 
