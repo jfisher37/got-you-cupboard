d64476f8a81da50399b7d094a96b8f4c08b9b8a4
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-22 13:21:18 -0400

    added marker for each search result

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,14 @@
 var mapquestAPIKey = "HPByterGQDhFFn4BqKh67908PJoXqDAf";
 var cityName = "philadelphia";
-var requestURL = "https://www.mapquestapi.com/search/v2/radius?origin=" + cityName + "&radius=1&maxMatches=10&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|541105&outFormat=json&key=" + mapquestAPIKey;
+var searchRadius = 5;
+var numOfResults = 100;
+var requestURL = "https://www.mapquestapi.com/search/v2/radius?origin=" + cityName + "&radius=" + searchRadius + "&maxMatches=" + numOfResults + "&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|541105&outFormat=json&key=" + mapquestAPIKey;
 
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
         .then(function (data) {
-            // console.log(data);
             generateMap(data);
         })
 
@@ -25,7 +26,23 @@ function generateMap(data) {
     
     //adds a marker to the map
     L.marker([latOfSearch, lngOfSearch], {
-        icon: L.mapquest.icons.marker(),
+        icon: L.mapquest.icons.marker({ 
+            primaryColor: '#22407F', 
+            size: 'lg'
+        }),
         draggable: false
-    }).bindPopup('Philadelphia').addTo(map);
+    }).bindPopup(data.origin.adminArea5).addTo(map);
+
+    //adds marker for each search result
+    var searchResults = data.searchResults;
+    for (let i = 0; i < searchResults.length; i++) {
+        var popupText = searchResults[i].name + " | " + searchResults[i].fields.address;
+        var groceryStoreLat = searchResults[i].fields.disp_lat;
+        var groceryStoreLng = searchResults[i].fields.disp_lng;
+
+        L.marker([groceryStoreLat, groceryStoreLng], {
+            icon: L.mapquest.icons.marker(),
+            draggable: false
+        }).bindPopup(popupText).addTo(map);
+    }
 }
